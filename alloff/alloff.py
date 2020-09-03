7118375b17a769b1a99aa8985489c982cabe9bd1
Jon Fincher <jfincher42@gmail.com>
2020-09-03 15:36:22 -0500

    Starting changes to read puzzle files

--- alloff/alloff.py
+++ alloff/alloff.py
@@ -5,20 +5,27 @@ AllOff - A Python version of the handheld game `Lights Out`
 
 import PySimpleGUI as sg
 import ui
+import json
 
 
-def setup_game_board():
-    # Init everything off
+def load_puzzles(puzzle_file):
+    # Open the file with pointers to our puzzle files
+    with open("puzzle-files.json", "r") as puzzle_filenames:
+        puzzle_files = json.load(puzzle_filenames)
+
+    # Get the location of our puzzle files
+    with open(puzzle_files[puzzle_file], "r") as puzzle_input:
+        puzzle_datafiles = json.load(puzzle_input)
+
+    # Open and read each file into our puzzle dictionary
+    # TODO: Make this an object instead?
+
+
+def setup_game_board(puzzle_file, level, puzzle):
+    # Init all the lights off
     board = [[False for col in range(5)] for row in range(5)]
 
-    # Get the new setup
-    update_board(board, 0, 0)
-    update_board(board, 0, 2)
-    update_board(board, 0, 4)
-    update_board(board, 4, 0)
-    update_board(board, 4, 2)
-    update_board(board, 4, 4)
-
+    # TODO: Get the puzzle requested
     return board
 
 
@@ -61,8 +68,17 @@ def update_display(board, window):
 if __name__ == "__main__":
     window = sg.Window("All Off!", ui.layout, finalize=True)
 
+    # Read the local configuration file
+    with open("config.json", "r") as config_file:
+        config = json.load(config_file)
+
+    # Load all current puzzles
+    puzzles = load_puzzles(config["current_puzzle_file"])
+
     # Setup game board
-    board = setup_game_board()
+    board = setup_game_board(
+        config["current_level"], config["current_puzzle"],
+    )
 
     # Show the initial setup
     update_display(board, window)
@@ -74,10 +90,17 @@ if __name__ == "__main__":
         if event == sg.WIN_CLOSED or event == "Exit":
             running = False
 
+        elif event == "-RESET-":
+            print("RESET")
+            board = setup_game_board()
+            window["-RESET-"].update(disabled=True)
+
         elif "-BUTTON-" in event:
             # Button press
             row, col = int(event[8]), int(event[10])
             update_board(board, row, col)
+            window["-RESET-"].update(disabled=False)
+            print(f"[{row},{col}]")
 
         update_display(board, window)
 
